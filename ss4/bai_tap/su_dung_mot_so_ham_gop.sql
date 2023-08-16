cbb18b27a455fb1a0b309ed5d2bada5f9787b9c5
thanhhaiqb234 <thanhhaiqb234@gimal.com>
2023-08-16 10:39:56 +0700

    nop bai tap

--- ss4/bai_tap/su_dung_mot_so_ham_gop.sql
+++ ss4/bai_tap/su_dung_mot_so_ham_gop.sql
@@ -7,11 +7,21 @@ count(borrows.book_id) as so_luong_cac_dau_sach_da_duoc_muon
 from books 
 join borrows
 on borrows.book_id = books.id
-group by books.title
+group by books.id
 order by count(borrows.book_id) desc;
 
--- - Thông kê các đầu sách chưa được mượn :	
 
+-- - Thông kê các đầu sách chưa được mượn :	
+select 
+books.id ,
+books.title ,
+books.page_size,
+books.author_id ,
+books.category_id
+from books
+where books.id not in 
+(select borrows.book_id
+from borrows);
 
 -- - Lấy ra danh sách các học viên đã từng mượn sách và sắp xếp  theo số lượng mượn sách từ lớn đến nhỏ	:
 select 
@@ -20,7 +30,7 @@ count(students.id)as so_lan_muon_sach_cua
 from students
 join borrows
 on borrows.student_id = students.id
-group by borrows.student_id
+group by students.id
 order by count(borrows.student_id) desc;
 
 -- - Lấy ra các học viên mượn sách nhiều nhất của thư viện	:
