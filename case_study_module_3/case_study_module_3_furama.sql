27b2a138425eb033c057ea34573267e3d062b333
thanhhaiqb234 <thanhhaiqb234@gimal.com>
2023-08-17 10:21:55 +0700

    nop bai tap

--- case_study_module_3/case_study_module_3_furama.sql
+++ case_study_module_3/case_study_module_3_furama.sql
@@ -431,3 +431,54 @@ limit 2;
 -- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
 -- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung
 --  (được tính dựa trên việc count các ma_dich_vu_di_kem).
+select 
+hd.ma_hop_dong ,
+ldv.ten_loai_dich_vu ,
+dvdk.ten_dich_vu_di_kem ,
+count(*) as so_lan_su_dung
+from hop_dong hd
+join dich_vu dv
+on dv.ma_dich_vu = hd.ma_hop_dong
+join loai_dich_vu ldv
+on ldv.ma_dich_vu = dv.ma_loai_dich_vu
+join hop_dong_chi_tiet hdct
+on hdct.ma_hop_dong = hd.ma_hop_dong
+join dich_vu_di_kem dvdk
+on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+group by hdct.ma_dich_vu_di_kem
+having count(hdct.ma_dich_vu_di_kem) = 1 ;
+
+
+-- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, 
+-- ten_bo_phan, so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+select 
+nv.ma_nhan_vien ,
+nv.ho_ten ,
+td.ten_trinh_do ,
+bp.ten_bo_phan ,
+nv.so_dien_thoai ,
+nv.dia_chi , 
+count(*) as so_lan_lap_hop_dong
+from nhan_vien nv
+join trinh_do td
+on td.ma_trinh_do = nv.ma_trinh_do
+join bo_phan bp
+on bp.ma_bo_phan = nv.ma_bo_phan
+join hop_dong hd
+on hd.ma_nhan_vien = nv.ma_nhan_vien
+where year(hd.ngay_lam_hop_dong) between 2020 and 2021
+group by hd.ma_nhan_vien
+having count(hd.ma_nhan_vien) <= 3
+order by nv.ma_nhan_vien;
+
+-- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
+alter table nhan_vien add trang_thai int default 0;
+select * 
+from nhan_vien nv
+join hop_dong hd
+on hd.ma_nhan_vien = nv.ma_nhan_vien
+where nv.ma_nhan_vien not in 
+(select 
+hd.ma_nhan_vien
+from hop_dong hd
+where year() );
