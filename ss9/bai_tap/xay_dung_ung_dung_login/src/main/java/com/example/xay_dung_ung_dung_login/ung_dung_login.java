84738a323479afcd27790a808d8206c1ab63c83b
thanhhaiqb234 <thanhhaiqb234@gimal.com>
2023-08-24 14:08:13 +0700

    nop bai tap

---
+++ ss9/bai_tap/xay_dung_ung_dung_login/src/main/java/com/example/xay_dung_ung_dung_login/ung_dung_login.java
@@ -0,0 +1,18 @@
+package com.example.xay_dung_ung_dung_login;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ung_dung_login", value = "/ung_dung_login")
+public class ung_dung_login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
